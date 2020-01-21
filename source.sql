03c13b1d8c5880ade66ba6c8111f9288b52988d8
EmilieLamps <emilie.lamps@gmail.com>
2020-01-21 11:55:58 +0100

    premier envoi

---
+++ source.sql
@@ -0,0 +1,35 @@
+-- INSERT INTO Animal
+-- VALUES (NULL , 'chat', 'F', '2010-09-13 15:02:00', 'Schtroumpfette', NULL);
+
+-- Insérez les données suivantes dans la table languages de la base webDevelopment :
+--     JavaScript, version 5
+--     PHP, version 5.2
+--     PHP, version 5.4
+--     HTML, version 5.1
+--     JavaScript, version 6
+--     JavaScript, version 7
+--     JavaScript, version 8
+--     PHP, version 7
+
+INSERT INTO `languages`
+VALUES (NULL , 'JavaScript' , 'version 5'),
+(NULL , 'PHP' , 'version 5.2'),
+(NULL , 'PHP' , 'version 5.4'),
+(NULL , 'HTML' , 'version 5.1'),
+(NULL , 'JavaScript' , 'version 6'),
+(NULL , 'JavaScript' , 'version 7'),
+(NULL , 'JavaScript' , 'version 8'),
+(NULL , 'PHP' , 'version 7');
+--  SELECT * FROM `languages`; pour afficher le contenu de la table languages
+
+
+-- Insérez les données suivantes dans la table frameworks de la base webDevelopment :
+--     Symfony, version 2.8
+--     Symfony, version 3
+--     Jquery, version 1.6
+--     Jquery, version 2.10
+INSERT INTO `frameworks`
+VALUES (NULL , 'Symfony' , 'version 2.8'),
+(NULL , 'Symfony' , 'version 3'),
+(NULL , 'JQuery' , 'version 1.6'),
+(NULL , 'JQuery' , 'version 2.10');
